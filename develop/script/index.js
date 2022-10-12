66fa3998c14a00320a68ed61dff6f21dcbd47161
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-12 16:26:52 -0700

    Added logic to save data.  Cannot test because require doesnt work.

--- develop/script/index.js
+++ develop/script/index.js
@@ -1,6 +1,7 @@
 const $buttons = document.getElementById(`buttonWrap`);
-const revolvingStudentsArray = [];
-let dangerZone = ``;
+const studentLoop = [];
+const fs = require(`fs`);
+
 //there are 24 people total in the class
 class Student {
     constructor(name){
@@ -8,22 +9,34 @@ class Student {
     };
 };
 
-Student.prototype.add = function(array){
-    array.push(this);
-};
 
 function checkRSALength(){
-    if(revolvingStudentsArray.length === 24){
-        dangerZone = revolvingStudentsArray.shift()
-        console.log(dangerZone)
-        console.log(revolvingStudentsArray)
+    if(studentLoop.length === 24){
+        const d = new Date();
+        const loopData = {
+            loop: studentLoop,
+            date: d.getTime()
+        };
+        //const strData = JSON.stringify(loopData);
+        fs.readFile(`../db/loopLogs.json`, `utf-8`, (err, data) =>{
+            if(err){
+                console.log(err);
+            }else if(data){
+                const prevData = JSON.parse(data);
+                const newOldData = prevData.push(loopData);
+                const strData = JSON.stringify(newOldData);
+                fs.writeFile(`../db/loopLogs.json`, strData, `utf-8`, (err) => err?console.log(err):console.log(`Added data to JSON file`))
+            }else{
+                const strData = JSON.stringify(loopData);
+                fs.writeFile(`../db/loopLogs.json`, strData, `utf-8`, (err) => err?console.log(err):console.log(`Added data to JSON file`))
+            }
+        })
     };
 };
 
 function addToRSA(e){
     let target = new Student(e.target.getAttribute`data-name`);
-    target.add(revolvingStudentsArray);
-    console.log(revolvingStudentsArray);
+    studentLoop.push(target);
     checkRSALength();
 };
 
