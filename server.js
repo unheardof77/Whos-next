9cd5b8a00a3dea28ea7469a8486d3df9286b51b0
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-13 09:02:27 -0700

    Got data to be sent to server in a readable format.  Safe revert point.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require(`fs`);
 const PORT = 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
