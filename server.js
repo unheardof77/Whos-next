3b2fc93a29bbc752193f7a479441185ef8d81ce2
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-13 17:34:29 -0700

    Integrated logic into random name genorater to allow for accurate test.  Safe revert point.

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get(`/randomnamegenerator`, (req, res)=> res.sendFile(path.join(__dirname, `public/mockRandomNameGen/index.html`)));
 
 app.post(`/submitlog`, (req, res) =>{
     console.info(`${req.method} request received to submit data.`);
     fs.readFile(`./db/loopLogs.json`, (err, data) => {
         if(err){
-            throw err;
+            throw new Error(err);
         }else{
             const json = JSON.parse(data);
             json.push(req.body);
@@ -22,7 +23,23 @@ app.post(`/submitlog`, (req, res) =>{
             fs.writeFile(`./db/loopLogs.json`, newData, (err) => err? console.error(err): console.log(`Appended data.`)) 
         };
     })
-    return res.status
+    res.status(200).json(`It worked.`);
 });
+
+app.post(`/submitlograndom`, (req, res) =>{
+    console.info(`${req.method} request received to submit data to test logs.`);
+    fs.readFile(`./db/testloopLogs.json`, (err, data) => {
+        if(err){
+            throw new Error(err);
+        }else{
+            const json = JSON.parse(data);
+            json.push(req.body);
+            const newData = JSON.stringify(json, null, 1);
+            fs.writeFile(`./db/testloopLogs.json`, newData, (err) => err? console.error(err): console.log(`Appended data to test logs.`)) 
+        };
+    })
+    res.status(200).json(`It worked.`);
+});
+
 
 app.listen(PORT, () =>console.log(`App listening at http://localhost:${PORT}`));
