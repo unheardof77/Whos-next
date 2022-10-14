28d0c48719f8cf561f65566585c3d722981e3d44
Toacin <toacinp@outlook.com>
2022-10-13 18:37:00 -0700

    now shows morgan's chances being called on next

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,6 @@
 const $buttons = document.getElementById(`buttonWrap`);
 const $allButtons = document.querySelectorAll(`button`);
+const $chance = document.querySelector(`span`)
 let studentLoop = [];
 
 //there are 24 people total in the class
@@ -8,6 +9,8 @@ class Student {
         this.name = name;
     };
 };
+
+calcOdds = () => $chance.textContent = ((1/(24-studentLoop.length))*100).toFixed(2);
 
 function checkRSALength(){
     if(studentLoop.length === 24){
@@ -26,6 +29,7 @@ function checkRSALength(){
         })
         .catch((err)=> console.error(err))
         studentLoop = []
+        calcOdds();
         for(i=0; i < $allButtons.length; i++){
             $allButtons[i].style.display = "inline-block";
         };
@@ -37,7 +41,15 @@ function addToRSA(e){
     e.target.style.display = "none"
     studentLoop.push(target);
     checkRSALength();
+    for (i=0; i<studentLoop.length; i++) {
+        if (studentLoop[i].name === "Morgan") {
+            $chance.textContent = 0;
+            return;
+        } else {
+            calcOdds();
+        }
+    }
 };
 
 $buttons.addEventListener(`click`, addToRSA)
-
+calcOdds();
